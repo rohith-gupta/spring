94b9ca69844d130dff66a06eb270d248add34178
rohith-gupta <rmarri@my.bridgeport.edu>
2025-02-01 12:32:57 -0500

    using @Autowired Annotation with diff types of injections

---
+++ src/main/java/com/spring/annotationautowire/ClassB.java
@@ -0,0 +1,57 @@
+package com.spring.annotationautowire;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class ClassB {
+	
+	// autowired here is used as property injection (byName)
+	ClassA a;
+	String lastname;
+	
+	public ClassA getA() {
+		return a;
+	}
+
+	// autowired here is used as setter injection (byName)
+	public void setA(ClassA A) {
+		this.a = A;
+	}
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	
+	
+	public ClassB() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	// autowired here is used as constructor injection (constructor)
+	@Autowired
+	public ClassB(ClassA a) {
+		super();
+		this.a = a;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ClassB [A=" + a + "]";
+	}
+	
+	
+	
+
+}
