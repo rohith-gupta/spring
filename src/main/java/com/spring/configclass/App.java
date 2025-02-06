cf192e52925d0cad6cdb7998182c9e87e8d98d0e
rohith-gupta <rmarri@my.bridgeport.edu>
2025-02-06 09:04:33 -0500

    Removing xml and using config class with @Configuration Annotation

---
+++ src/main/java/com/spring/configclass/App.java
@@ -0,0 +1,23 @@
+package com.spring.configclass;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	ApplicationContext context = new AnnotationConfigApplicationContext(Configclass.class);
+    	
+    	ClassB b = (ClassB) context.getBean("classB");
+    	
+    	System.out.println(b.getA().getName()+" "+ b.getLastname());
+    	
+    }
+}
---
+++ src/main/java/com/spring/configclass/Configclass.java
@@ -0,0 +1,12 @@
+package com.spring.configclass;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring.configclass")
+public class Configclass {
+	
+	
+
+}
